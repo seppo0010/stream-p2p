54dbe10c8e1185a376a190aff933ae4b7fa2841e
Sebastian Waisbrot <seppo0010@gmail.com>
2020-05-03 11:52:30 -0300

    Register streamer in signaling channel

--- index.js
+++ index.js
@@ -2,17 +2,30 @@ const fs = require('fs');
 const http = require('http');
 const url = require('url');
 
+let waitingStreamers = {};
 const staticFiles = ['picker', 'play', 'stream'].map((file) => {
-  return ['/' + (file === 'picker' ? '' : file), fs.readFileSync(`${file}.html`, 'utf-8')]
+  return [(file === 'picker' ? '' : file), fs.readFileSync(`${file}.html`, 'utf-8')]
 }).reduce((acc, val) => { acc[val[0]] = val[1]; return acc }, {})
 
 const server = http.createServer((req, res) => {
   const u = new url.URL(req.url, `http://${req.headers.host}`);
-  if (staticFiles[u.pathname]) {
-    res.end(staticFiles[u.pathname])
+  const segments = u.pathname.split('/')
+  const firstSegment = segments[1]
+  if (req.method === 'GET' && staticFiles[firstSegment]) {
+    res.end(staticFiles[firstSegment])
     return;
   }
-  switch (u.path) {
+  if (req.method === 'POST' && firstSegment === 'stream') {
+    let body = '';
+    req.setEncoding('utf8');
+    req.on('data', chunk => {
+        body += chunk;
+    });
+    req.on('end', () => {
+      waitingStreamers[segments[2]] = body;
+      res.end()
+    });
+    return;
   }
   res.writeHead(404)
   res.end()
