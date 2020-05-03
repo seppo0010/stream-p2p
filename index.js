38239aa10864645e83d6f0ccb6397341985d9cca
Sebastian Waisbrot <seppo0010@gmail.com>
2020-05-03 12:35:30 -0300

    One connection working

--- index.js
+++ index.js
@@ -3,7 +3,8 @@ const http = require('http');
 const url = require('url');
 
 let waitingStreamers = {};
-const staticFiles = ['picker', 'play', 'stream'].map((file) => {
+let streamerWaitingForAnswer = {}
+const staticFiles = ['picker', 'player', 'streamer'].map((file) => {
   return [(file === 'picker' ? '' : file), fs.readFileSync(`${file}.html`, 'utf-8')]
 }).reduce((acc, val) => { acc[val[0]] = val[1]; return acc }, {})
 
@@ -11,11 +12,15 @@ const server = http.createServer((req, res) => {
   const u = new url.URL(req.url, `http://${req.headers.host}`);
   const segments = u.pathname.split('/')
   const firstSegment = segments[1]
+  if (req.method === 'GET' && firstSegment === 'robots.txt') {
+    res.end('User-agent: *\nDisallow: /')
+    return;
+  }
   if (req.method === 'GET' && staticFiles[firstSegment]) {
     res.end(staticFiles[firstSegment])
     return;
   }
-  if (req.method === 'POST' && firstSegment === 'stream') {
+  if (req.method === 'POST' && firstSegment === 'stream' && segments.length === 3) {
     let body = '';
     req.setEncoding('utf8');
     req.on('data', chunk => {
@@ -23,8 +28,28 @@ const server = http.createServer((req, res) => {
     });
     req.on('end', () => {
       waitingStreamers[segments[2]] = body;
+      res.writeHead(204)
       res.end()
     });
+    return;
+  }
+
+  if (req.method === 'GET' && firstSegment === 'stream' && waitingStreamers[segments[2]] && segments.length === 3) {
+    res.writeHead(200, {'content-type': 'application/json'})
+    res.end(waitingStreamers[segments[2]])
+    delete waitingStreamers[segments[2]]
+    return;
+  }
+  if (req.method === 'POST' && firstSegment === 'stream' && segments.length === 4 && segments[3] === 'answer' && streamerWaitingForAnswer[segments[2]]) {
+    const otherRes = streamerWaitingForAnswer[segments[2]]
+    otherRes.writeHead(200, {'content-type': 'application/json'})
+    req.pipe(otherRes)
+    res.writeHead(204)
+    res.end()
+    return;
+  }
+  if (req.method === 'GET' && firstSegment === 'stream' && segments.length === 4 && segments[3] === 'answer') {
+    streamerWaitingForAnswer[segments[2]] = res;
     return;
   }
   res.writeHead(404)
